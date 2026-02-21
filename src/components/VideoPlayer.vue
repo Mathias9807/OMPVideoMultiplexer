96d1087ca36eedf7a0bd7d12ab93cbcee2b45565
Mathias Johansson <mathias9807@gmail.com>
2026-02-21 13:43:37 +0100

    Fix mouse hover overlay

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -31,13 +31,13 @@
 </script>
 
 <template>
-  <div class="container">
+  <div class="container" @mouseover="showOverlay = true" @mouseleave="showOverlay = false">
     <OvenPlayerVue3
       ref="ovenplayer"
       :config="playerConfig"
       @error="errorHandler" />
 
-    <div :class="['overlay', { 'overlay--visible': showOverlay }]" @mouseover="showOverlay = true" @mouseleave="showOverlay = false">
+    <div :class="['overlay', { 'overlay--visible': showOverlay }]">
       <div class="close-button" @click="$emit('close')">
         <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" id="mdi-close" viewBox="0 0 24 24"><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg>
       </div>
