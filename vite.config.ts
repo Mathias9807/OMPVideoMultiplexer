9fa418ac046ce1dcfe114597b493835ef448624b
Mathias Johansson <mathias9807@gmail.com>
2026-02-22 10:31:46 +0100

    Add source maps

--- vite.config.ts
+++ vite.config.ts
@@ -14,30 +14,9 @@ export default defineConfig({
   ],
   server: {
     allowedHosts: ['ameno.local'],
-    proxy: {
-      '/ome-ws': {
-        target: 'https://stream.kirr.nu',
-        ws: true,
-        changeOrigin: true,
-        secure: true,
-        headers: {
-          origin: 'null',
-          host: 'stream.kirr.nu',
-        },
-        rewrite: (path) => path.replace(/^\/ome-ws/, ''),
-        configure: (proxy) => {
-          proxy.on('proxyReqWs', (proxyReq) => {
-            proxyReq.setHeader('origin', 'null');
-            proxyReq.setHeader('sec-websocket-origin', 'null');
-            proxyReq.setHeader('host', 'stream.kirr.nu');
-          });
-
-          proxy.on('error', (error) => {
-            console.error('[ome-ws proxy error]', error);
-          });
-        },
-      },
-    },
+  },
+  build: {
+    sourcemap: true,
   },
   resolve: {
     alias: {
